f4e3b14acb7141df161dccaec57ba54193de825e
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2024-02-04 21:09:33 -0600

    wip

--- platform/build.sh
+++ platform/build.sh
@@ -3,8 +3,6 @@ roc build ../main.roc --lib --output libapp.so
 # Why does it append 1.0 to the output?
 mv libapp.so.1.0 libapp.so
 
-cargo build
+cp libapp.so target/debug/libapp.so.1
 
-mv libapp.so target/debug/libapp.so.1
-
-cargo run
+cargo lambda build
--- platform/src/main.rs
+++ platform/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    std::process::exit(host::rust_main() as _);
+// fn main() {
+//     std::process::exit(host::rust_main() as _);
+// }
+
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct Request {
+    command: String,
+}
+
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
+}
+
+async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    let command = event.payload.command;
+
+    host::rust_main();
+
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {}.", command),
+    };
+
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
 }
